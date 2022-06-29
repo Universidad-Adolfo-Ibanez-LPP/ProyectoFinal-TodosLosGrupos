8e78634f59a6dba3424f60fd048fae95f911d002
Casantibanezm <101525888+Casantibanezm@users.noreply.github.com>
2022-06-28 22:33:51 -0400

    Update main_funciones.py

--- Grupo 6/Logical/main_funciones.py
+++ Grupo 6/Logical/main_funciones.py
@@ -22,6 +22,7 @@ desv_estandar=attach_price_to_drugstore(desv_estandar)
 
 busqueda_mas_caro=[]
 busqueda_mas_caro=mas_caro_por_busqueda(busqueda_mas_caro,dic_datos,0)
+mayor_a_1UF=mayor_que_uno(dic_datos)
 
 dic_to_csv(titles,caro,"Remedio_mas_caro_por_farmacia")
 dic_to_csv(titles,menor_a_mil,"Cuestan_menos_de_1000CLP")
@@ -29,6 +30,6 @@ dic_to_csv(titles,mas_barato_buscados,"Mas_barato_por_busqueda")
 dic_to_csv(titlesespfarmacia,promedio_cada_farmacia,"Precio_promedio_total_farmacia")  
 dic_to_csv(titlesespfarmacia,desv_estandar,"Desviacion_estandar_de_farmacias")  
 dic_to_csv(titles,busqueda_mas_caro,"Mas_caro_por_busqueda")
-
+dic_to_csv(titles,mayor_a_1UF,"Cuestan_mas_de_1UF")
 
 print("Hecho")  
