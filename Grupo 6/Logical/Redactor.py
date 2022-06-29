ad3db437ccd225f0ba59d41648fedab0bbeb3329
cristobalsepc <67245093+cristobalsepc@users.noreply.github.com>
2022-06-28 22:53:02 -0400

    Update Redactor.py

--- Grupo 6/Logical/Redactor.py
+++ Grupo 6/Logical/Redactor.py
@@ -4,15 +4,15 @@ def csv_to_dic(titles):
   with open('out.csv', newline='') as f:
       reader = csv.reader(f)
       data = list(reader)
-  
+
   data.pop(0)
   dic_datos=[]
-  
+
   for row in data:
     iterator=zip(titles, row)
     a_dictionary = dict(iterator)
     dic_datos.append(a_dictionary)
-  
+
   return dic_datos
 
 
@@ -26,8 +26,26 @@ def attach_price_to_drugstore(list):
   farmacias=["Ahumada", "Salcobrand", "Redfarma"]
   lista=[]
   for i in range(len(farmacias)):
-  
+
     diccionari={"farmacia": farmacias[i], "precio_promedio":list[i]}
     lista.append(diccionari)
   return lista
-  
+
+
+def attach_price_to_drugstoredesv(list):
+  farmacias=["Ahumada", "Salcobrand", "Redfarma"]
+  lista=[]
+  for i in range(len(farmacias)):
+
+    diccionari={"farmacia": farmacias[i], "desviacion_estandar":list[i]}
+    lista.append(diccionari)
+  return lista
+
+def attach_price_to_drugstorevar(list):
+  farmacias=["Ahumada", "Salcobrand", "Redfarma"]
+  lista=[]
+  for i in range(len(farmacias)):
+
+    diccionari={"farmacia": farmacias[i], "varianza":list[i]}
+    lista.append(diccionari)
+  return lista
